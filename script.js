48695a006294b047315cdcf4c1977052a7693a10
Nwankwor Benjamin <Benjaminnwankwor2109@gmail.com>
2025-05-01 13:08:11 +0100

    Added more things that i learnt from W3 Schools

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (hasDriversLicense) console.log('I can Drive :D');
 //const interface = 'Audio';
 //const private = 534;/
 //const if = 23;
-*/
+
 
 
 function logger() {
@@ -33,3 +33,32 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+cars = ["Toyota", "Volvo", "Audi"];    // ERROR
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+const car = {type:"Fiat", model:"500", color:"white"};
+
+car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
+
+*/
