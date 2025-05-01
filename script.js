264d02c4a923ec809b87c720c84d0614e48ba59b
Nwankwor Benjamin <Benjaminnwankwor2109@gmail.com>
2025-05-01 13:59:39 +0100

    Committed

--- script.js
+++ script.js
@@ -61,4 +61,41 @@ const car = {type:"Fiat", model:"500", color:"white"};
 
 car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
 
+// Function declaration.
+
+function calcAge1(birthYear) {
+ return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+ 
+// function expression
+const calcAge2 = function (birthYear) {
+ return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+// arrow function
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+ const age =  2037 - birthYear;
+ const retirement = 65 - age;
+ //return retirement;
+ return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'))
+//console.log(`Mr Stephan has ${yearsUntilRetirement(1991)} years till he retires.`);
 */
+
+function fruitProcessor(apples, oranges) {
+ const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+ return juice;
+}
